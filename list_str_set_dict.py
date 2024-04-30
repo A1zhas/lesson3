fde62cd1bde13f94411efb99a06f9b06de754c8f
A1zhas <ovopleasure@gmail.com>
2024-04-30 20:57:08 +0500

    add project

---
+++ list_str_set_dict.py
@@ -0,0 +1,11 @@
+#list
+'list.append(), list.count(), list.sort(), list.reverse(), list.copy()'
+
+#str
+'S.find(), S.index(), S.split(), S.upper(), S.lower()'
+
+#set
+'set.copy(), set.update(), set.add(), set.remove(), set.clear()'
+
+#dict
+'dict.clear(), dict.copy(), dict.get(), dict.items(), dict.keys()'
